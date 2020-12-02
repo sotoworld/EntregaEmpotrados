f6068fd186d8251934a841d4c11dce872c19b73b
Sotoworld <sotoworld@hotmail.com>
2020-12-02 14:06:11 +0100

    valores-imagenes
    valores por defecto e imágenes a usar

--- app/src/main/java/com/example/entregaempotrados/MainActivity.java
+++ app/src/main/java/com/example/entregaempotrados/MainActivity.java
@@ -1,8 +1,26 @@
 package com.example.entregaempotrados;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
